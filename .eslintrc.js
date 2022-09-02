6fbaf48099b9eacc09a7019fc1229f4fd7f88aa4
Vikas2426 <vikas.cby@gmail.com>
2022-09-03 01:21:31 +0530

    feat: add eslint and styled-components

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
